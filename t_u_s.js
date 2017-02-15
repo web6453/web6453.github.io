95007e51fc43372469b60c3aba447cc5017bf6f4
web6453 <dl6453@gmail.com>
2017-02-16 00:36:35 +0200

    Initial commit

---
+++ t_u_s.js
@@ -0,0 +1,51 @@
+
+var str = prompt('введи доменное имя', '');
+while(str !='12345') {
+	if (str == 'lysenkods') break;
+	else if (str == 'zheka2110') break;
+	else if (str == 'danila') break;
+	else if (str == 'hondak') break;
+	else if (str == 'Shahvlad') break;
+	else if (str == 'vic725') break;
+	else if (str == 'zheka2110') break;
+	else if (str == 'danila') break;
+	else if (str == 'hondak') break;
+	else if (str == 'Shahvlad') break;
+	else if (str == 'vic725') break;
+	else if (str == 'pvl') break;
+	else if (str == 'budnik') break;
+	else if (str == 'Kislyak') break;
+	else if (str == 'Kocherga') break;
+	else if (str == 'gonchar_molodoy') break;
+	else if (str == 'popoff') break;
+	else if (str == 'walkman') break;
+	else if (str == 'Mongol9') break;
+	else if (str == 'Sokolov') break;
+	else if (str == 'discloser') break;
+	else if (str == 'gadomskiy') break;
+	else if (str == 'scherbina') break;
+	else if (str == 'pistonit') break;
+	else if (str == 'kattion') break;
+	else if (str == 'boris_2') break;
+	else if (str == 'varan') break;
+	else if (str == 'dikiyzver10') break;
+	else if (str == 'lagutin') break;
+	else if (str == 'lizogubov') break;
+	else if (str == 'strigforever') break;
+	else if (str == 'ternovskystas') break;
+	else if (str == 'kalinicenko') break;
+	else if (str == 'ivasenko') break;
+	else if (str == 'laktionoff') break;
+	else if (str == 'kamen') break;
+	else if (str == 'Jernov') break;
+	else if (str == 'vladimirch') break;
+	else if (str == 'laktionov') break;
+	else if (str == 'vasvla') break;
+	str = prompt('введенное имя не корректно или у Вас нет доступа для просмотра данной страницы, введи другое доменное имя', ''); 
+}
+
+function allClick() {
+  $('.hid').click()
+};
+
+
